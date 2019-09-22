fd501e416235eea8bdcabecaaba746fb35dc8b0a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 16:46:23 +0000

    Deploy carlosmaniero/ddd-domain-event-react-minesweeper to github.com/carlosmaniero/ddd-domain-event-react-minesweeper.git:gh-pages

---
+++ precache-manifest.461d638af236e88ecae738a7125f62f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708bc4fa1652d05fefe7af3c636416ce",
+    "url": "/ddd-domain-event-react-minesweeper/index.html"
+  },
+  {
+    "revision": "dcf74a06d113574163e2",
+    "url": "/ddd-domain-event-react-minesweeper/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "009f239e1f64729ff569",
+    "url": "/ddd-domain-event-react-minesweeper/static/js/2.445aaeb8.chunk.js"
+  },
+  {
+    "revision": "dcf74a06d113574163e2",
+    "url": "/ddd-domain-event-react-minesweeper/static/js/main.8cc3e3be.chunk.js"
+  },
+  {
+    "revision": "0d65a419077626c0511a",
+    "url": "/ddd-domain-event-react-minesweeper/static/js/runtime-main.74b69b05.js"
+  }
+]);
